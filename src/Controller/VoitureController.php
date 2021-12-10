f1622203f83011b03ccbe1e27852fb5201e2c827
aymenb97 <ayman.eightb@gmail.com>
2021-12-10 18:30:13 +0100

    form voiture

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -6,6 +6,7 @@ use App\Entity\Voiture;
 use App\Form\VoitureForm;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 class VoitureController extends AbstractController
@@ -41,5 +42,39 @@ class VoitureController extends AbstractController
     return $this->render('voiture/addVoiture.html.twig',[
         'form_voiture'=>$form->createView()
     ]);
+    }
+
+    /**
+     * @Route("/delete-voiture/{id}", name="voitureDelete")
+     */
+    public  function deleteVoiture($id):Response
+    {
+
+        $em=$this->getDoctrine()->getManager();
+        $voiture= $em->getRepository("App\Entity\Voiture")->find($id);
+        if($voiture !== null){
+            $em->remove($voiture);
+            $em->flush();
+        }else{
+        throw new NotFoundHttpException("La voiture ".$id." n'existe pas");
+        }
+        return $this->redirectToRoute("voiture");
+
+    }
+    /**
+     * @Route ("updateVoiture/{id}",name="voitureUpdate")
+     */
+    public function  updateVoiture(Request $request, $id):Response
+    {
+        $em= $this->getDoctrine()->getManager();
+        $voiture = $em->getRepository("App\Entity\Voiture")->find($id);
+        $editForm = $this->createForm(VoitureForm::class,$voiture);
+        $editForm->handleRequest($request);
+        if($editForm->isSubmitted() and $editForm->isValid()){
+            $em->persist($voiture);
+            $em->flush();
+            return $this->redirectToRoute("voiture");
+        }
+        return $this->render('voiture/updateVoiture.html.twig',['editFormVoiture'=>$editForm->createView()]);
     }
 }
